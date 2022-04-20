c2f3b6d50efe544fe8f53fcc341956f6dba213f2
levko0303 <90134056+levko0303@users.noreply.github.com>
2022-04-20 23:47:58 +0300

    Create menu.cs

---
+++ menu.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace Lab_1
+{
+    class Input
+    {
+        public static string input_id()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Please input the id: \n");
+                    int number = int.Parse(Console.ReadLine());
+                    if (number <= 0)
+                    {
+                        Console.WriteLine("ID can't be negative: ");
+                        continue;
+                    }
+                    return Convert.ToString(number);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+        }
+
+        public static string input_file_name()
+        {
+            while (true)
+                try
+                {
+                    string name = Console.ReadLine();
+                    string path = "E:\\навчальна практика\\programlab1\\programlab1\\";
+                    string file_name = path + name;
+                    if (!File.Exists(file_name))
+                    {
+                        Console.WriteLine("File doesn't exist");
+                        continue;
+                    }
+                    return file_name;
+                }
+                catch (FileNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+        }
+
+        public static string field()
+        {
+
+            Console.WriteLine("Please, input the field to sort elements by it: \n");
+            string field = Console.ReadLine();
+            return field;
+
+        }
+    }
+}
+
