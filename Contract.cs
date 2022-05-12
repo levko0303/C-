17dcf96744aa9e9f1438608fa12c949f74968434
levko0303 <90134056+levko0303@users.noreply.github.com>
2022-05-12 03:59:01 +0300

    Create Contract.cs

---
+++ Contract.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models2
+{
+    public class Contract
+    {
+        public int ContractId { get; set; }
+        public string ContractName { get; set; }
+        public string ContractEmail { get; set; }
+        public string ContractPhone { get; set; }
+        public string ContractIBAN { get; set; }
+        public string ContractStartDate { get; set; }
+        public string ContractDueDate { get; set; }
+    }
+}
